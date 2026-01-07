90b2fe30c7be4e053715c76f324d803e27358a05
Michal Švarc <michal.svarc@reality.co>
2026-01-07 18:27:06 +0100

    fix: update icon purpose in manifest to remove 'maskable' specification

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -19,13 +19,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: `${basePath}/icon-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
       {
         src: `${basePath}/icon-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
     ],
   }
