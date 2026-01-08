cf6cd1bfc904811075f01c2705d82490b84fbf18
Michal Švarc <michal.svarc@reality.co>
2026-01-08 14:31:36 +0100

    chore: remove unnecessary viewport properties from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  themeColor: '#1976d2',
 };
 
 export default function RootLayout({
